4ee1d2fc27a27e8a4cb74600160e35fa635da8f8
Mayur Jagtap <mayurjagtap9112@gmail.com>
2024-02-24 23:00:40 +0530

    Update frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,20 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { mode } from "@chakra-ui/theme-tools";
+import { extendTheme } from "@chakra-ui/theme-utils";
+import { ColorModeScript } from "@chakra-ui/color-mode";
 import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 const styles = {
   global: (props) => ({
     body: {
-      color: props.colorMode === "light" ? "gray800" : "whiteAlpha.900",
-      bg: props.colorMode === "light" ? "gray.100" : "#101010",
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("gray.100", "#101010")(props),
     },
   }),
 };
 
 const config = {
-  initialColorMode: "light",
+  initialColorMode: "dark",
   useSystemColorMode: true,
 };
 
@@ -29,12 +34,17 @@ const colors = {
 const theme = extendTheme({ config, styles, colors });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // React.StrictMode renders every component twice (in the initial render), only in development.
   <React.StrictMode>
-    <BrowserRouter>
-      <ChakraProvider theme={theme}>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-        <App />
-      </ChakraProvider>
-    </BrowserRouter>
+    <RecoilRoot>
+      <BrowserRouter>
+        <ChakraProvider theme={theme}>
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+          <SocketContextProvider>
+            <App />
+          </SocketContextProvider>
+        </ChakraProvider>
+      </BrowserRouter>
+    </RecoilRoot>
   </React.StrictMode>
 );
